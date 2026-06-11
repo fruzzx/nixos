4c7cb63d6f045cd3abd096c73b0e9944f63fcef7
fruzzx <290776880+fruzzx@users.noreply.github.com>
2026-06-11 21:18:46 +0200

    small fix

--- modules/user/plasma.nix
+++ modules/user/plasma.nix
@@ -1,9 +1,11 @@
 {
   flake.nixosModules.plasma = {
-    services.desktopManager.plasma6.enable = true;
-    services.displayManager = {
-      plasma-login-manager.enable = true;
-      autoLogin.user = "fruzzx";
+    services = {
+      desktopManager.plasma6.enable = true;
+      displayManager = {
+        plasma-login-manager.enable = true;
+        autoLogin.user = "fruzzx";
+      };
     };
   };
 }
